ef2ddb3f11839ef16e77f0da0f7c75f0b593ed88
RCS111 <jaycyme@gmail.com>
2022-01-22 11:18:20 +0800

    added peso

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -5,7 +5,7 @@ function MenuItem({ image, name, price }) {
     <div className="menuItem">
       <div style={{ backgroundImage: `url(${image})` }}> </div>
       <h1> {name} </h1>
-      <p> ₹{price} </p>
+      <p> {price}php </p>
     </div>
   );
 }

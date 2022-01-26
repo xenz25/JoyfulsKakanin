554d5445bfc4cc006383ced49c657695c0e7a565
RCS111 <jaycyme@gmail.com>
2022-01-26 14:40:12 +0800

    fixed bugs

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,19 +11,12 @@ function About() {
       <div className="aboutBottom">
         <h1> ABOUT US</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
-          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
-          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
-          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
-          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
-          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
-          officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
-          cumque velit
+          Joyful's Kakanin is a food business for original kapampangan kakanin
+          recipes.<br/>
+          Some of the products include, Maja Blanca or Tibok-Tibok in kapampangan, Sapin-Sapin, Turon, Minatamis na Langka and many more,
+          which can be purchase in Tubs or in different sizes of Bilao.<br />
+          The prices are affordable making it perfect for events, or as a gift for
+          your loved ones.
         </p>
       </div>
     </div>
